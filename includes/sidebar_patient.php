ce53259de2dab64e6a22bc8561a557ffc75560c8
daveynmdz <diaz.316506@koronadal.sti.edu.ph>
2025-09-17 19:10:19 +0800

    Update sidebar_patient.php

--- includes/sidebar_patient.php
+++ includes/sidebar_patient.php
@@ -106,7 +106,7 @@ if (($needsName || $needsNo) && $patient_id) {
             <div class="user-info">
                 <img class="profile-photo"
                     src="<?= $patient_id
-                                ? '../vendor/photo_controller.php?patient_id=' . urlencode((string)$patient_id)
+                                ? '/wbhsms-cho-koronadal/vendor/photo_controller.php?patient_id=' . urlencode((string)$patient_id)
                                 : 'https://ik.imagekit.io/wbhsmslogo/user.png?updatedAt=1750423429172' ?>"
                     alt="User photo"
                     onerror="this.onerror=null;this.src='https://ik.imagekit.io/wbhsmslogo/user.png?updatedAt=1750423429172';">
